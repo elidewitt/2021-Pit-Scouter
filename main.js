67d1195f3711e67b16a84ff2369e1b67db21d32d
Eli DeWitt <65370615+elidewitt@users.noreply.github.com>
2020-09-17 20:08:54 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+for (i in pitScouter) {
+  tab = addItem("div");
+  tab.id = i;
+  tab.style.backgroundColor = "hsl(" + Math.random()*360 + ", 100%, 50%)";
+  tabHeader = tab.appendChild(addItem("h1", i));
+  for (j in pitScouter[i]) {
+    card = tab.appendChild(addItem("div"));
+    cardHeader = card.appendChild(addItem("h2", j));
+    for (k in pitScouter[i][j]) {
+      p = card.appendChild(addItem("p", k));
+      input = card.appendChild(addItem(pitScouter[i][j][k].element, ""));
+      input.type = pitScouter[i][j][k].type;
+    }
+  }
+}
+addItem("button", "Submit");
 
+
+
+
+function addItem(tag, innerHTML) {
+  elt = document.createElement(tag);
+  if (innerHTML) {
+    elt.appendChild(document.createTextNode(innerHTML));
+  }
+  return document.getElementById("scouter").appendChild(elt);
+}
--- source.js
+++ source.js
@@ -1,1 +1,108 @@
-
+const pitScouter = {
+  "Robot Information" : {
+    "Robot Size" : {
+      "Height (Inches)" : {
+        "element" : "input",
+        "type" : "number"
+      },
+      "Weight (Pounds)" : {
+        "element" : "input",
+        "type" : "number"
+      }
+    },
+    "Drivetrain" : {
+      "What type?" : {
+        "element" : "select",
+        "type" : "text"
+      }
+    },
+    "Inspection" : {
+      "Did they pass inspection?" : {
+        "element" : "input",
+        "type" : "checkbox"
+      }
+    },
+    "Vision System" : {
+      "What type?" : {
+        "element" : "select",
+        "type" : "text"
+      },
+      "How is it used?" : {
+        "element" : "input",
+        "type" : "text"
+      }
+    }
+  },
+  "Autonomous" : {
+    "Autonomous Strategy" : {
+      "Prefered Starting Place" : {
+        "element" : "select",
+        "type" : "text"
+      },
+      "Strategy?" : {
+        "element" : "select",
+        "type" : "text"
+      }
+    },
+    "Autonomous Shooting" : {
+      "Prefered location?" : {
+        "element" : "select",
+        "type" : "text"
+      },
+      "Power port(s)?" : {
+        "element" : "select",
+        "type" : "text"
+      }
+    }
+  },
+  "Tele-Op" : {
+    "Tele-Op Shooting" : {
+      "Prefered location?" : {
+        "element" : "select",
+        "type" : "text"
+      },
+      "Power port(s)?" : {
+        "element" : "select",
+        "type" : "text"
+      }
+    },
+    "Control Panel" : {
+      "Rotate control?" : {
+        "element" : "input",
+        "type" : "checkbox"
+      },
+      "Position control" : {
+        "element" : "input",
+        "type" : "checkbox"
+      }
+    }
+  },
+  "Other" : {
+    "Power Cell Management" : {
+      "Max cells at one time?" : {
+        "element" : "input",
+        "type" : "number"
+      },
+      "How do they load cells?" : {
+        "element" : "select",
+        "type" : "text"
+      }
+    },
+    "End Game" : {
+      "Can they Hang?" : {
+        "element" : "input",
+        "type" : "checkbox"
+      },
+      "Leveling method (for multiple hanging bots)?" : {
+        "element" : "input",
+        "type" : "text"
+      }
+    },
+    "Comments" : {
+      "" : {
+        "element" : "input",
+        "type" : "text"
+      }
+    }
+  },
+}
