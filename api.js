b9e6d53225152faa80b35647af4702c26e4ad86e
Eli DeWitt <65370615+elidewitt@users.noreply.github.com>
2021-01-23 14:35:20 -0600

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-//
-let schedule;
-fetch("https://frc-api.firstinspires.org/v2.0/2019/schedule/MNDU?tournamentLevel=qual")
-  .then(response => response.json())
-  .then(data => {
-    schedule = data.Schedule;
-  })
-
-// schedule[matchNum - 1].teams
